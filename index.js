1122d107cab9ae340f3b367c44b32f26af8584b5
Louiza Mak <louiza.mak@gmail.com>
2024-01-03 15:42:43 -0600

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(driverArray, driverName) {
+    return driverArray.filter(driverElement => driverElement.toUpperCase() === driverName.toUpperCase());
+}
+
+function fuzzyMatch(driverArray, driverName) {
+    let nameLength = driverName.length;
+    return driverArray.filter(driverElement => driverElement.toUpperCase().substring(0, nameLength) === driverName.toUpperCase());
+}
+
+function matchName(driverArray, driverName) {
+    return driverArray.filter(driverElement => driverElement.name === driverName);
+}
